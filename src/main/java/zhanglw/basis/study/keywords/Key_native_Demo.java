9d1fd7c98acf5fa0fb57357d0b0b278b35290479
ting723 <ting723@gmail.com>
2016-12-20 00:34:08 +0800

    native

--- src/main/java/zhanglw/basis/study/keywords/Key_native_Demo.java
+++ src/main/java/zhanglw/basis/study/keywords/Key_native_Demo.java
@@ -2,20 +2,43 @@ package zhanglw.basis.study.keywords;
 
 /**
  * Created by zhanglw on 2016/12/18.
- *
  */
 public class Key_native_Demo {
 
     private static String HellNative_DLL = "dll/zhanglw_basis_study_keywords_Key_native_Demo.dll";
 
     static {
-        String dllFile= Key_native_Demo.class.getResource("/").getPath()+ HellNative_DLL;
+        String dllFile = Key_native_Demo.class.getResource("/").getPath() + HellNative_DLL;
         System.load(dllFile);
     }
 
     public static native void say();
 
+    /**
+     * 可以返回非基本类型
+     *
+     * @return
+     */
+    public native Key_native_Demo getDemo();
+
+    /**
+     * 可以抛出异常
+     *
+     * @return
+     * @throws Exception
+     */
+    public native String getEx() throws Exception;
+
+    /**
+     * native 可以被子类用java方法重写
+     */
+    class TestNative extends Key_native_Demo {
+        public Key_native_Demo getDemo() {
+            return null;
+        }
+    }
+
     public static void main(String[] args) {
-//        say();
+        say();
     }
 }
