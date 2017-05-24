71d7e4050d3f851c54a2d9d2d3f5e1e703be8db4
ting723 <ting723@gmail.com>
2017-05-24 11:07:13 +0800

    update

--- src/main/java/zhanglw/basis/study/keywords/Key_final_demo.java
+++ src/main/java/zhanglw/basis/study/keywords/Key_final_demo.java
@@ -3,5 +3,15 @@ package zhanglw.basis.study.keywords;
 /**
  * Created by zhanglianwei on 2017/5/9.
  */
-public class Key_final_demo {
+public final class Key_final_demo {
+
+    private final int a = 0;
+
+    private final String ab = "";
+
+
+    private final void testFianlMethod() {
+
+    }
+
 }
--- src/main/java/zhanglw/basis/study/other/StringDemo.java
+++ src/main/java/zhanglw/basis/study/other/StringDemo.java
@@ -4,4 +4,16 @@ package zhanglw.basis.study.other;
  * Created by zhanglianwei on 2017/5/11.
  */
 public class StringDemo {
+
+    public static void main(String[] args) {
+        String a = "He";
+        String b = "llo";
+        String ab = "Hello";
+        String c = a + b;
+        final String d = "Hello";
+        System.out.println((a + b) == c);
+        System.out.println((a + b) == ab);
+        System.out.println(c == ab);
+        System.out.println(d == ab);
+    }
 }
